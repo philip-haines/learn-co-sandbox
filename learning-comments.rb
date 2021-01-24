7f4c388cf5a97891133e3189fb939a9ac991609b
Phil Haines <phil.haines92@gmail.com>
2021-01-24 20:37:08 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,34 @@
+# From the Three Dof Night song: "Joy to the world"
+puts "Joy to the world"
+# puts "all the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+# name = "Byron"
+name = "Luca"
+
+puts "We're sorry #{name}, you are not allowed into the store."
+
+name = "Byron"
+# name = "Luca"
+
+puts "We're sorry #{name}, you are not allowed into the store."
+
+puts
+puts
+puts
+puts "-------Variations on If / Else Statement Notes Below--------"
+puts
+puts
+puts
+
+current_weather = "raining"
+
+case current_weather 
+  when "sunny"
+    puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrealla"
+  when "snowing"
+    puts "bundle up"
+end 
---
+++ method_notes.rb
@@ -0,0 +1,5 @@
+def say_hello_world
+  puts "Hello World!"
+end
+
+say_hello_world
